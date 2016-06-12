6742eed356d1a7544b86340282de2abf35741c7b
mcelearr <rorymcelearney@gmail.com>
2016-06-12 16:43:15 +0100

    converts to honours board from top score board

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 var database =
 [
-  { "name": "Rory", "top_score": 10, "colour": "red" },
-  { "name": "Troy", "top_score": 9, "colour": "blue" },
-  { "name": "Rich", "top_score": 13, "colour": "green" },
-  { "name": "Matt", "top_score": 12, "colour": "yellow" }
+  { "name": "Rich", "score": 13, "date": "01/06/16", "colour": "green" },
+  { "name": "Rory", "score": 10, "date": "01/06/16", "colour": "red" },
+  { "name": "Matt", "score": 12, "date": "02/06/16", "colour": "yellow" }
 ]
--- table.js
+++ table.js
@@ -4,15 +4,19 @@ var createTable = function(){
   var headerRow = table.insertRow();
   var header1 = headerRow.insertCell(0);
   var header2 = headerRow.insertCell(1);
+  var header3 = headerRow.insertCell(2);
   header1.innerHTML = "Player Name";
-  header2.innerHTML = "Top Score";
+  header2.innerHTML = "Score";
+  header3.innerHTML = "Date";
 
   for (var i = 0; i < database.length; i++) {
     var row = table.insertRow();
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
     cell1.innerHTML = database[i].name;
-    cell2.innerHTML = database[i].top_score;
+    cell2.innerHTML = database[i].score;
+    cell3.innerHTML = database[i].date;
   }
 }
 
