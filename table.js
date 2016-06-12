6e0fc613dfe47c7ace6151471952cd0885c00c49
mcelearr <rorymcelearney@gmail.com>
2016-06-12 17:02:25 +0100

    creates table header section and body section separately

--- table.js
+++ table.js
@@ -1,7 +1,8 @@
 var createTable = function(){
   var table = document.getElementById("data-table");
 
-  var headerRow = table.insertRow();
+  var header = table.createTHead();
+  var headerRow = header.insertRow();
   var header1 = headerRow.insertCell(0);
   var header2 = headerRow.insertCell(1);
   var header3 = headerRow.insertCell(2);
@@ -9,8 +10,10 @@ var createTable = function(){
   header2.innerHTML = "Score";
   header3.innerHTML = "Date";
 
+  var body = table.appendChild(document.createElement('tbody'));
+
   for (var i = 0; i < database.length; i++) {
-    var row = table.insertRow();
+    var row = body.insertRow();
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
