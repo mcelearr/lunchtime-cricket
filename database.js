2c3468ea6eb2713eab2ceb5120915327e60ea020
mcelearr <rorymcelearney@gmail.com>
2016-06-12 15:38:56 +0100

    sets up table.js to dynamically create table and fixes database error

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var database =
 [
-  { name: "Rory", top score: "10", colour: "red" },
-  { name: "Troy", top score: "9", colour: "blue" },
-  { name: "Rich", top score: "13", colour: "green" },
-  { name: "Matt", top score: "12", colour: "yellow" }
+  { "name": "Rory", "top score": 10, "colour": "red" },
+  { "name": "Troy", "top score": 9, "colour": "blue" },
+  { "name": "Rich", "top score": 13, "colour": "green" },
+  { "name": "Matt", "top score": 12, "colour": "yellow" }
 ]
---
+++ table.js
@@ -0,0 +1,6 @@
+var createRow = function(player){
+  var tableRow = document.getElementById(player);
+  console.log(tableRow)
+}
+
+createRow("player-1")
