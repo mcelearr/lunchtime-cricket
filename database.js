ad77bc0d44c7d02c8c690e39e33f7be8bead15e9
mcelearr <rorymcelearney@gmail.com>
2016-06-12 16:34:09 +0100

    dynamically creates table of top scores

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var database =
 [
-  { "name": "Rory", "top score": 10, "colour": "red" },
-  { "name": "Troy", "top score": 9, "colour": "blue" },
-  { "name": "Rich", "top score": 13, "colour": "green" },
-  { "name": "Matt", "top score": 12, "colour": "yellow" }
+  { "name": "Rory", "top_score": 10, "colour": "red" },
+  { "name": "Troy", "top_score": 9, "colour": "blue" },
+  { "name": "Rich", "top_score": 13, "colour": "green" },
+  { "name": "Matt", "top_score": 12, "colour": "yellow" }
 ]
--- table.js
+++ table.js
@@ -1,6 +1,19 @@
-var createRow = function(player){
-  var tableRow = document.getElementById(player);
-  console.log(tableRow)
+var createTable = function(){
+  var table = document.getElementById("data-table");
+
+  var headerRow = table.insertRow();
+  var header1 = headerRow.insertCell(0);
+  var header2 = headerRow.insertCell(1);
+  header1.innerHTML = "Player Name";
+  header2.innerHTML = "Top Score";
+
+  for (var i = 0; i < database.length; i++) {
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = database[i].name;
+    cell2.innerHTML = database[i].top_score;
+  }
 }
 
-createRow("player-1")
+createTable();
