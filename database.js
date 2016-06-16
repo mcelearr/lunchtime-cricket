ece0e4f14f8c3ced0c4e174c1eeca3903578fdbf
mcelearr <rorymcelearney@gmail.com>
2016-06-16 09:57:11 +0100

    update wall of shame function

--- database.js
+++ database.js
@@ -7,3 +7,10 @@ var honoursBoard =
   { "name": "Rory", "score": 10, "date": "14/06/16", "colour": "red" },
   { "name": "Rory", "score": 12, "date": "14/06/16", "colour": "red" }
 ]
+
+var wallOfShame =
+[
+  { "name": "Rich", "shame": "Dropped catches", "week": "Week 1" },
+  { "name": "Troy", "shame": "Choked on 9", "week": "Week 1" },
+  { "name": "Rory", "shame": "Bowled out twice", "week": "Week 1" }
+]
--- table.js
+++ table.js
@@ -32,19 +32,19 @@ var createWallOfShame = function(){
   var header2 = headerRow.insertCell(1);
   var header3 = headerRow.insertCell(2);
   header1.innerHTML = "Player Name";
-  header2.innerHTML = "Score";
-  header3.innerHTML = "Date";
+  header2.innerHTML = "Shame";
+  header3.innerHTML = "Week";
 
   var body = table.appendChild(document.createElement('tbody'));
 
-  for (var i = 0; i < honoursBoard.length; i++) {
+  for (var i = 0; i < wallOfShame.length; i++) {
     var row = body.insertRow();
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
-    cell1.innerHTML = honoursBoard[i].name;
-    cell2.innerHTML = honoursBoard[i].score;
-    cell3.innerHTML = honoursBoard[i].date;
+    cell1.innerHTML = wallOfShame[i].name;
+    cell2.innerHTML = wallOfShame[i].shame;
+    cell3.innerHTML = wallOfShame[i].week;
   }
 }
 
